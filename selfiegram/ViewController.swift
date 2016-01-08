9ddb584f4a94d0d2b331ceefe6e953f4ec355df0
astopo <amy.soetopo@gmx.com>
2016-01-07 18:55:28 -0800

    Initial Commit

---
+++ selfiegram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selfiegram
+//
+//  Created by stopo on 2016-01-07.
+//  Copyright © 2016 stopo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
