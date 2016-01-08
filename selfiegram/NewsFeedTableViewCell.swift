e37e4ffa64a8b017e563014c57a0de862f22dd73
astopo <amy.soetopo@gmx.com>
2016-01-07 20:31:53 -0800

    newsfeedtablecell

---
+++ selfiegram/NewsFeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsFeedTableViewCell.swift
+//  selfiegram
+//
+//  Created by stopo on 2016-01-07.
+//  Copyright © 2016 stopo. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
