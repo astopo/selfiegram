23e57b3698d82038f5e126e37a2e1679af5a063f
astopo <amy.soetopo@gmx.com>
2016-01-12 20:05:40 -0800

    add map kit to map tab, not adding my pin tho

--- selfiegram/MapViewController.swift
+++ selfiegram/MapViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let location = CLLocationCoordinate2D(
+            latitude: 51.50007773,
+            longitude: -0.1246402
+        )
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        map.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        map.addAnnotation(annotation)
+        print("LOADED")
     }
 
     override func didReceiveMemoryWarning() {
