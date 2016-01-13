992cd92e0793a7f7944e7f45bc8965847b3ebf80
astopo <amy.soetopo@gmx.com>
2016-01-12 19:35:30 -0800

    it works, yay

--- selfiegram/ProfileViewController.swift
+++ selfiegram/ProfileViewController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
         usernameLabel.text = "AMY!"
     }
 
@@ -28,6 +30,37 @@ class ProfileViewController: UIViewController {
     @IBAction func cameraButtonPressed(sender: AnyObject) {
         usernameLabel.text = "You pressed the button"
         print("Camera butotn pressed")
+        
+        // 1. Create an ImagePickerController
+        let pickerController = UIImagePickerController()
+        
+        // 2: Self in this line refers to this View Controller
+        //  Setting the Delegate Property means you want to receive a message from pickerController when a specific event is triggered.
+        pickerController.delegate = self
+        
+        if TARGET_OS_SIMULATOR == 1 {
+            // 3. We check if we are funning on a Simulator
+            //    If so, we pick a photo from the simulator's Photo Library. We need to do this because the simlator does not have a camera
+            pickerController.sourceType = .PhotoLibrary
+        } else {
+            pickerController.sourceType = .Camera
+            pickerController.cameraDevice = .Front
+            pickerController.cameraCaptureMode = .Photo
+        }
+        
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // 1. When the delegate method is returned, it passes along a dictionary called info
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            profileImageView.image = image
+            print("let worked")
+        } else {
+            print("let did not work")
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
 
     /*
