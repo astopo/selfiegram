26a4d49b13d0118e5ae31c44e2a19528e737cd07
astopo <amy.soetopo@gmx.com>
2016-01-26 19:09:49 -0800

    add image, name and comment

---
+++ SelfieCell.swift
@@ -0,0 +1,27 @@
+//
+//  SelfieCell.swift
+//  selfiegram
+//
+//  Created by stopo on 2016-01-26.
+//  Copyright © 2016 stopo. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var selfieImageView: UIImageView!
+    @IBOutlet weak var contentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
