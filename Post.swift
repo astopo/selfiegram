86a1d7de549b8c0c00055d4842ae521deba45bce
astopo <amy.soetopo@gmx.com>
2016-02-05 11:18:43 -0800

    store user activity

--- Post.swift
+++ Post.swift
@@ -15,6 +15,13 @@ class Post: PFObject, PFSubclassing {
     @NSManaged var user:PFUser
     @NSManaged var comment:String
     
+    var likes: PFRelation! {
+        // PFRelations are a bit different from other properties
+        // This is called a “computed property”, because it’s value is computed every time instead of stored.
+        // The line below specifies that our relation column on Parse.com should be called “likes”
+        return relationForKey("likes")
+    }
+    
     // convenience init method, because it’s building on top of PFObject’s init, rather than overriding it.
     convenience init(image:PFFile, user:PFUser, comment:String){
         // You can name the property you are passing into the function the
