2b7c571a65327b06e931466b69f3713e7b24e7e6
astopo <amy.soetopo@gmx.com>
2016-02-05 10:10:53 -0800

    add button action to feed view ctrl

--- FeedViewController.swift
+++ FeedViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Parse
 
-class FeedViewController: UITableViewController {
+class FeedViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var posts = [Post]()
 
@@ -50,7 +51,53 @@ class FeedViewController: UITableViewController {
     }
 
     @IBAction func cameraButtonPressed(sender: AnyObject) {
+        // 1. Create an ImagePickerController
+        let pickerController = UIImagePickerController()
+        
+        // 2: Self in this line refers to this View Controller
+        //  Setting the Delegate Property means you want to receive a message from pickerController when a specific event is triggered.
+        pickerController.delegate = self
+        
+        if TARGET_OS_SIMULATOR == 1 {
+            // 3. We check if we are funning on a Simulator
+            //    If so, we pick a photo from the simulator's Photo Library. We need to do this because the simlator does not have a camera
+            pickerController.sourceType = .PhotoLibrary
+        } else {
+            pickerController.sourceType = .Camera
+            pickerController.cameraDevice = .Front
+            pickerController.cameraCaptureMode = .Photo
+        }
+        
+        self.presentViewController(pickerController, animated: true, completion: nil)
+
     }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            // setting the compression quality to 90%
+            if let imageData = UIImageJPEGRepresentation(image, 0.9),
+                let imageFile = PFFile(data: imageData),
+                let user = PFUser.currentUser(){
+                    
+                    //2. We create a Post object from the image
+                    let post = Post(image: imageFile, user: user, comment: "A Selfie")
+                    
+                    post.saveInBackgroundWithBlock({ (success, error) -> Void in
+                        if success {
+                            print("Post successfully saved in Parse")
+                            
+                            // self.posts.insert(post, atIndex: 0)
+                            
+                        }
+                    })
+            }
+        }
+        
+        //3. We remember to dismiss the Image Picker from our screen.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
 
     /*
     // Override to support conditional editing of the table view.
